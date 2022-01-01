1913c7b7b3138c671cd42a97270d89565d1b97f5
Tobi <tobisaid007@gmail.com>
2022-01-01 20:18:42 +0100

    01/01/22

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -31,6 +31,16 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 //@path      POST api/v1/bootcamps
 //@access    Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    //add user to req.body
+    req.body.user = req.user.id;
+
+    //check for published bootcamp
+    const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+    //check if user is an admin
+    if (publishedBootcamp && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`The user with id ${req.user.id} has already published a bootcamp`, 400))
+    }
 
     const bootcamp = await Bootcamp.create(req.body);
     res.status(201).json({ success: true, data: bootcamp });
